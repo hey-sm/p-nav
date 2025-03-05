06e89a462d9d8adaf2511acaf7f4a452fadb6c76
hey <1760967618@qq.com>
2025-03-05 18:54:25 +0800

    feat  构建规则

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/** @format */
+
+module.exports = {
+    rules: {
+        '@typescript-eslint/no-empty-interface': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn'
+    }
+}
